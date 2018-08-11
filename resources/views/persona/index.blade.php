f5d14d4a04bc8289417725df31dbec447f4fe6a1
juan jose hernandez <jhernandez@sistema.com.gt>
2018-08-10 21:35:42 -0700

    select de personas

---
+++ resources/views/persona/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+@foreach ($dat as $user)
+    <p>Nombre {{ $user->nombre }} {{$user->apellido}}</p>
+@endforeach
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::resource('/persona','PersonaController');
 Route::get('/', function () {
     return view('welcome');
 });
